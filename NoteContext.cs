f048e666b5cd83f91716cf39b13bb466e64af922
Riley Lin <75247434+FakeStandard@users.noreply.github.com>
2022-10-29 17:16:18 +0800

    Create NoteContext.cs

---
+++ NoteContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using net6_note_system.Models;
+
+namespace net6_note_system
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            var conn = @"Data Source=(Localdb)\MSSQLLocalDB;Initial Catalog=NoteSystem;Integrated Security=True";
+            builder.UseSqlServer(conn);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Note>().ToTable("Note");
+        }
+    }
+}
