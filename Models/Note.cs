2baa417e4178c9cafe4e9a45ebdcf8e1600f5165
Riley Lin <75247434+FakeStandard@users.noreply.github.com>
2022-10-29 17:16:04 +0800

    Create Note Model

---
+++ Models/Note.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace net6_note_system.Models
+{
+    public class Note
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public int CreateUser { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public int? ModifyUser { get; set; }
+
+        public DateTime? ModifyTime { get; set; }
+    }
+}
