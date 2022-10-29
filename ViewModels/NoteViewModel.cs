9cac022999769c54905c072fa755a27dfe21fa30
Riley Lin <75247434+FakeStandard@users.noreply.github.com>
2022-10-29 17:16:13 +0800

    Create Note ViewModel

---
+++ ViewModels/NoteViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace net6_note_system.ViewModels
+{
+    public class NoteViewModel
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("標題")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayName("內容")]
+        public string Content { get; set; }
+    }
+}
