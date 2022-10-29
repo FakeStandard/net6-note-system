3a980dc039505b2ac4688200c763b1f1a5c5b963
Riley Lin <75247434+FakeStandard@users.noreply.github.com>
2022-10-29 17:16:52 +0800

    Add Note Controller

---
+++ Controllers/NoteController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net6_note_system.DAL.IRepository;
+using net6_note_system.ViewModels;
+
+namespace net6_note_system.Controllers
+{
+    public class NoteController : Controller
+    {
+        private INoteRepository _repository;
+        public NoteController(INoteRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var notes = _repository.GetListAsync();
+
+            return View(notes);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(NoteViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _repository.AddAsync(new Models.Note
+            {
+                Title = model.Title,
+                Content = model.Content,
+                CreateUser = 1,
+                CreateTime = DateTime.Now
+            });
+
+            return RedirectToAction("Index");
+        }
+    }
+}
