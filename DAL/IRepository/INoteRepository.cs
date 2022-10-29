a8f88c8f78240d28a6c92ccf141a89674ee81831
Riley Lin <75247434+FakeStandard@users.noreply.github.com>
2022-10-29 17:16:34 +0800

    Add Repository

---
+++ DAL/IRepository/INoteRepository.cs
@@ -0,0 +1,12 @@
+﻿using net6_note_system.Models;
+
+namespace net6_note_system.DAL.IRepository
+{
+    public interface INoteRepository
+    {
+        Task<List<Note>> GetListAsync();
+        Task<Note> GetByIdAsync(int id);
+        Task AddAsync(Note note);
+        Task UpdateAsync(Note note);
+    }
+}
---
+++ DAL/Repository/NoteRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using net6_note_system.DAL.IRepository;
+using net6_note_system.Models;
+
+namespace net6_note_system.DAL.Repository
+{
+    public class NoteRepository : INoteRepository
+    {
+        private NoteContext _context;
+
+        public NoteRepository(NoteContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<Note>> GetListAsync()
+            => _context.Notes.ToListAsync();
+
+        public Task<Note> GetByIdAsync(int id)
+            => _context.Notes.FirstOrDefaultAsync(c => c.ID == id);
+
+        public Task AddAsync(Note note)
+        {
+            _context.Notes.Add(note);
+            return _context.SaveChangesAsync();
+        }
+
+        public Task UpdateAsync(Note note)
+        {
+            _context.Entry(note).State = EntityState.Modified;
+            return _context.SaveChangesAsync();
+        }
+    }
+}
